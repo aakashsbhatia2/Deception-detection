22d6347dd709782e55ec3df88152f041f2c254b6
Aakash Bhatia <aakashsbhatia@outlook.com>
2020-05-13 19:15:34 -0400

    cleaned data

---
+++ clean_dataset.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+data_deceptive = []
+for i in range(1,6):
+    directory = 'deceptive_from_MTurk/fold'+str(i)
+    for filename in os.listdir(directory):
+        f = open(directory+"/"+filename, "r")
+        lines = f.read()
+        data_deceptive.append([lines, "-1.0"])
+    directory = 'truthful_from_TripAdvisor/fold' + str(i)
+    for filename in os.listdir(directory):
+        f = open(directory + "/" + filename, "r")
+        lines = f.read()
+        data_deceptive.append([lines, "1.0"])
+
+df = pd.DataFrame(data_deceptive, columns=["Sentence", "Label"])
+df.to_csv("final_data.csv", index=False)
