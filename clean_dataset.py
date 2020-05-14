d71181cd2113308cad779a63f06598e6bc2ddb2f
Aakash Bhatia <aakashsbhatia@outlook.com>
2020-05-13 20:33:46 -0400

    added folds

--- clean_dataset.py
+++ clean_dataset.py
@@ -7,12 +7,12 @@ for i in range(1,6):
     for filename in os.listdir(directory):
         f = open(directory+"/"+filename, "r")
         lines = f.read()
-        data_deceptive.append([lines, "-1.0"])
+        data_deceptive.append([lines, "-1.0", i])
     directory = 'truthful_from_TripAdvisor/fold' + str(i)
     for filename in os.listdir(directory):
         f = open(directory + "/" + filename, "r")
         lines = f.read()
-        data_deceptive.append([lines, "1.0"])
+        data_deceptive.append([lines, "1.0", i])
 
-df = pd.DataFrame(data_deceptive, columns=["Sentence", "Label"])
+df = pd.DataFrame(data_deceptive, columns=["Sentence", "Label", "Fold"])
 df.to_csv("final_data.csv", index=False)
