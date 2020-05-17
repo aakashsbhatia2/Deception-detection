28748ec6352c73e069a020b7547fad7d99492423
Nitin Bhyri <nitinkumarbhyri@gmail.com>
2020-05-16 22:29:10 -0700

    adding pipeline to execute multiple scenarios

--- deception_detection_svm.py
+++ deception_detection_svm.py
@@ -2,13 +2,15 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report
 from sklearn import svm
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
 
-def ngram_representation(train, test, vectorizer):
+def ngram_representation(train, test, pipeline):
     train_sentences = train["abstract"]
     test_sentences = test["abstract"]
-    train_features = vectorizer.fit_transform(train_sentences)
+    train_features = pipeline.fit_transform(train_sentences)
     train_labels = train["label"]
-    test_features = vectorizer.transform(test_sentences)
+    test_features = pipeline.transform(test_sentences)
     test_labels = test["label"]
     return train_features, train_labels, test_features, test_labels
 
@@ -32,20 +34,14 @@ def update_metrics(score, metrics = {}, report = None):
     metrics['f1_score_class_negative'] += report['0.0']['f1-score']
     return metrics
 
-def cross_validation(data, representation = "unigram"):
-    representation_map = {
-        "unigram": (1, 1),
-        "bigram": (1, 2),
-        "trigram": (1, 3)
-    }
-    data["abstract"].apply(lambda x: x.lower())
+def cross_validation(data, pipeline):
     folds = data["fold"].unique()
     metrics = update_metrics(0)
     for fold in folds:
         fold_train = data[data["fold"] != fold]
         fold_test = data[data["fold"] == fold]
-        vectorizer = CountVectorizer(ngram_range = representation_map[representation])
-        train_features, train_labels, test_features, test_labels = ngram_representation(fold_train, fold_test, vectorizer)
+        # vectorizer = CountVectorizer(ngram_range = representation_map[representation])
+        train_features, train_labels, test_features, test_labels = ngram_representation(fold_train, fold_test, pipeline)
         lin_clf = svm.LinearSVC()
         lin_clf.fit(train_features, train_labels)
         y_pred = lin_clf.predict(test_features)
@@ -53,22 +49,35 @@ def cross_validation(data, representation = "unigram"):
         report = classification_report(test_labels, y_pred, output_dict=True)
         metrics = update_metrics(score, metrics, report)
 
-    for metric in metrics:
-        metrics[metric] = metrics[metric] / len(folds)
-        print(f"{metric} : {metrics[metric]}")
+    print(f"Accuracy: {metrics['accuracy'] / len(folds)}")
+    print(f"P(Truthful): {metrics['precision_class_positive'] / len(folds)}, R(Truthful): {metrics['recall_class_positive'] / len(folds)}, F1(Truthful): {metrics['f1_score_class_positive'] / len(folds)}")
+    print(f"P(Deceptive): {metrics['precision_class_negative'] / len(folds)}, R(Deceptive): {metrics['recall_class_negative'] / len(folds)}, F1(Deceptive): {metrics['f1_score_class_negative'] / len(folds)}")
 
 
 if __name__ == "__main__":
     data = pd.read_csv("final_data.csv")
+    pipelines = {
+        "Unigram": Pipeline([('vect', CountVectorizer(ngram_range = (1, 1)))]),
+        "Bigram": Pipeline([('vect', CountVectorizer(ngram_range = (1, 2)))]),
+        "Trigram": Pipeline([('vect', CountVectorizer(ngram_range=(1, 3)))]),
+        "Unigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range = (1, 1)))]),
+        "Bigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range = (1, 2)))]),
+        "Trigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range=(1, 3)))]),
+        "Unigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 1))),
+            ('tfidf', TfidfTransformer())
+        ]),
+        "Bigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 2))),
+            ('tfidf', TfidfTransformer())
+        ]),
+        "Trigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 3))),
+            ('tfidf', TfidfTransformer())
+        ]),
+    }
+    for key in pipelines:
+        print(f"---------{key}----------")
+        cross_validation(data, pipelines[key])
 
-    print("------------Unigram Representation-----------")
-    cross_validation(data)
-    print("")
-
-    print("------------Bigram Representation------------")
-    cross_validation(data, "bigram")
-
-    print("")
-    print("------------Trigram Representation-----------")
-    cross_validation(data, "trigram")
 
--- naive-bayes.py
+++ naive-bayes.py
@@ -1,14 +1,15 @@
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
 
-def create_word_vector(X_train, X_test, ngram):
-    cv = CountVectorizer(strip_accents='ascii', token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', lowercase=True, stop_words='english', ngram_range=ngram)
+def create_word_vector(X_train, X_test, pipeline):
+    # cv = CountVectorizer(strip_accents='ascii', token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', lowercase=True, stop_words='english', ngram_range=ngram)
 
-    X_train_cv = cv.fit_transform(X_train)
-    X_test_cv = cv.transform(X_test)
+    X_train_cv = pipeline.fit_transform(X_train)
+    X_test_cv = pipeline.transform(X_test)
 
     return X_train_cv, X_test_cv
 
@@ -38,7 +39,7 @@ def update_metrics(score, metrics = {}, report = None):
     metrics['f1_score_class_negative'] += report['0.0']['f1-score']
     return metrics
 
-def cross_validation(data, ngram):
+def cross_validation(data, pipeline):
     data["abstract"].apply(lambda x: x.lower())
     folds = data["fold"].unique()
     metrics = update_metrics(0)
@@ -46,7 +47,7 @@ def cross_validation(data, ngram):
         fold_train = data[data["fold"] != fold]
         fold_test = data[data["fold"] == fold]
 
-        train_features, test_features = create_word_vector(fold_train['abstract'], fold_test['abstract'], ngram)
+        train_features, test_features = create_word_vector(fold_train['abstract'], fold_test['abstract'], pipeline)
         train_labels = fold_train['label']
         test_labels = fold_test['label']
 
@@ -58,24 +59,37 @@ def cross_validation(data, ngram):
         report = classification_report(test_labels, y_pred, output_dict=True)
         metrics = update_metrics(score, metrics, report)
 
-    for metric in metrics:
-        metrics[metric] = metrics[metric] / len(folds)
-        print(f"{metric} : {metrics[metric]}")
+    print(f"Accuracy: {metrics['accuracy'] / len(folds)}")
+    print(f"P(Truthful): {metrics['precision_class_positive'] / len(folds)}, R(Truthful): {metrics['recall_class_positive'] / len(folds)}, F1(Truthful): {metrics['f1_score_class_positive'] / len(folds)}")
+    print(f"P(Deceptive): {metrics['precision_class_negative'] / len(folds)}, R(Deceptive): {metrics['recall_class_negative'] / len(folds)}, F1(Deceptive): {metrics['f1_score_class_negative'] / len(folds)}")
 
 
 def main():
-    ngram_types = [["Unigram" , (1,1)], ["Bigram", (1,2)], ["Trigram", (2,3)]]
+    # ngram_types = [["Unigram" , (1,1)], ["Bigram", (1,2)], ["Trigram", (2,3)]]
     df = pd.read_csv("final_data.csv")
-
-    for ngram in ngram_types:
-        print(ngram[0])
-        cross_validation(df, ngram[1])
-
-        # print('Accuracy score: ', accuracy_score(y_test, predictions))
-        # print('Precision score: ', precision_score(y_test, predictions))
-        # print('Recall score: ', recall_score(y_test, predictions))
-        # print('Recall score: ', f1_score(y_test, predictions))
-        # print('************')
+    pipelines = {
+        "Unigram": Pipeline([('vect', CountVectorizer(ngram_range=(1, 1)))]),
+        "Bigram": Pipeline([('vect', CountVectorizer(ngram_range=(1, 2)))]),
+        "Trigram": Pipeline([('vect', CountVectorizer(ngram_range=(1, 3)))]),
+        "Unigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range=(1, 1)))]),
+        "Bigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range=(1, 2)))]),
+        "Trigram-without-stopwords": Pipeline([('vect', CountVectorizer(stop_words='english', ngram_range=(1, 3)))]),
+        "Unigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 1))),
+            ('tfidf', TfidfTransformer())
+        ]),
+        "Bigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 2))),
+            ('tfidf', TfidfTransformer())
+        ]),
+        "Trigram-Tfidf": Pipeline([
+            ('vect', CountVectorizer(ngram_range=(1, 3))),
+            ('tfidf', TfidfTransformer())
+        ]),
+    }
+    for key in pipelines:
+        print(f"---------{key}----------")
+        cross_validation(df, pipelines[key])
 
 if __name__ == "__main__":
     main()
